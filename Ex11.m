0d6460a0add2c4919b4d7f8942f440f23b21d9b4
Elias Assaf <elyas51000@gmail.com>
2021-10-30 14:17:19 +0300

    added comments

--- Ex11.m
+++ Ex11.m
@@ -1,6 +1,7 @@
 close all;
 disp('Elias Assaf 315284729 - Jameel Nassar 206985152')
 
+%note that most of the code is the formatting of graphs, which is self explanatory
 picasso = imread('picasso.jpg');
 picasso_grayscale = double(rgb2gray(picasso));
 picasso_normalized = dip_GN_imread('picasso.jpg');
@@ -361,21 +362,25 @@ section_25(square_normalized);
 function [gray_img_norm] = dip_GN_imread(file_name)
     img = imread(file_name);
     img_gray = double(rgb2gray(img));
+    %normalize according to the equation given in the ex1 pdf
     gray_img_norm = (img_gray - min(img_gray(:)))./ (max(img_gray(:)) - min(img_gray(:)));
 end
 
 function [counts, bins] = dip_histogram(img,nbins)
     bins = (0:(nbins-1))/(nbins-1);
     counts = zeros(nbins,1);
+    %calculate the bin range according to the matlab imhist description, taking into account image of type double
     for i = (1:nbins)
         low_b = (i-1.5)/(nbins-1);
         high_b = (i-0.5)/(nbins-1);
+        %for each bin count how many pixel in the image is in its range, and place it in counts array accordingly
         counts(i) = sum((img(:) >= low_b)&(img(:) < high_b));
     end
 
 end
 
 function [adj_img] = adjust_brightness(img, action, parameter) % 1.3
+    %func is the requested function to perform
     if action == "add"
         func = @(x,y) x + y;
     elseif action == "mul"
@@ -384,15 +389,19 @@ function [adj_img] = adjust_brightness(img, action, parameter) % 1.3
         error('Not a valid operator')
     end
     adj_img = func(img, parameter);
+    %clip the values if they become too big - grayscale images are from 0 to 1
     adj_img(adj_img > 1) = 1;
     adj_img(adj_img < 0) = 0;
 end
 
 function [adj_img] = adjust_contrast(img,range_low,range_high)
+    %normalize according to the low and high range
     adj_img = img * (range_high-range_low) + range_low;
 end
 
 function [quantized_img] = quantize_img(img,bits) % 1.5
+    %each pixel is from range 0-255 which is 0-(2^8)-1
+    %divide by 2^(8-bits), and we reduce the number range to the correct range of 0-(2^bits)-1
     quantized_img = floor(img ./ 2^(8-bits));
 end
 
@@ -403,6 +412,7 @@ function [filtered_img] = filter(img, k, filt)
     %same for the left and right columns
     filtered_img_x = [repmat(filtered_img_y(:, 1), 1, to_extend), filtered_img_y , repmat(filtered_img_y(:, end), 1, to_extend)];
     filtered_img = zeros(size(filtered_img_x));
+    %compute the convolution according to the equation seen in class
     for m=(1:size(img, 1))+to_extend
         for n=(1:size(img, 2))+to_extend
                 filtered_img(m,n) = sum(filt .* filtered_img_x((m:m+k-1)-to_extend, (n:n+k-1)-to_extend), 'all');
@@ -420,20 +430,24 @@ function [filtered_img] = mean_filter(img, k) %assume k is odd
     if mod(k,2) == 0
         error('filter size must be an odd number');
     end
+    %box filter as seen in class
     filt = 1/k^2 * ones(k);
     filtered_img = filter(img, k, filt);
 end
 
 function [filtered_img] = median_filter(img, k)
     to_extend = floor(k/2);
+    %pad the matrix as in filter function
     filtered_img_y = [repmat(img(1, :), to_extend, 1) ; img; repmat(img(end, :), to_extend, 1)];
     filtered_img_x = [repmat(filtered_img_y(:, 1), 1, to_extend), filtered_img_y , repmat(filtered_img_y(:, end), 1, to_extend)];
     filtered_img = zeros(size(filtered_img_x));
+    %for each coordinates m,n take a box matrix around the square and find the box median.
     for m=(1:size(img, 1))+to_extend
         for n=(1:size(img, 2))+to_extend
                 filtered_img(m,n) = median(filtered_img_x((m:m+k-1)-to_extend, (n:n+k-1)-to_extend), 'all');
         end
     end
+    %cut to original image size
     filtered_img = filtered_img( (1:size(img, 1))+to_extend, (1:size(img, 2))+to_extend);
 end
 
@@ -442,12 +456,14 @@ function [filtered_img] = dip_gaussian_filter(img, k, sigma) %assume k is odd
         error('filter size must be an odd number');
     end
     edges = floor(k/2);
+    %use meshgrid to create 2 coords matrixes to calculate the gaussian distribution function
     [coords_x, coords_y] = meshgrid(-edges:edges, -edges:edges);
     filt = 1/(2 * pi * sigma^2) * exp(-(coords_x.^2 + coords_y.^2) / (2 * sigma^2));
+    %use filter function
     filtered_img = filter(img, k, filt);
 end
 
-function [] = section_25(normalized_photo)
+function [] = section_25(normalized_photo) %draw alot of graphs
     noise_types = ["salt & pepper" "gaussian" "speckle"];
     for noise = noise_types
         noisy_dog = imnoise(normalized_photo, noise);
