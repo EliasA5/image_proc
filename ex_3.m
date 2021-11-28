17e1ca6622d08fbf8a7937a2f74eead25c2f0b76
Elias Assaf <elyas51000@gmail.com>
2021-11-28 21:22:42 +0200

    TODO: add explanations for ex3, and submit
    Co-authored-by: jameel978 <jameel978@users.noreply.github.com>

--- ex_3.m
+++ ex_3.m
@@ -42,8 +42,60 @@ Free_Willy(imprisoned_willy);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Section 1.2.2
 
+
+square_1 = zeros(128, 128);
+[square_1_X, square_1_y] = meshgrid(44:83, 44:83);
+idx_1 = sub2ind(size(square), square_1_X, square_1_y);
+square(idx_1) = 1;
+square_fft = fft2(square);
+figure;
+subplot(1,2,1);imshow(square);title("square 1")
+subplot(1,2,2);imshow(abs(dip_fftshift(square_fft)));title("square1 fft2")
+
+
+square_2 = zeros(128, 128);
+[square_2_X, square_2_y] = meshgrid(64:103, 64:103);
+idx_2 = sub2ind(size(square_2), square_2_X, square_2_y);
+square_2(idx_2) = 1;
+square_2_fft = fft2(square_2);
+figure;
+subplot(1,2,1);imshow(square_2);title("square 2");
+subplot(1,2,2);imshow(abs(dip_fftshift(square_2_fft)));title("square 2 fft2");
+figure;
+subplot(1,2,1);imshow(abs(dip_fftshift(square_fft)));title("square1 fft2")
+subplot(1,2,2);imshow(abs(dip_fftshift(square_2_fft)));title("square2 fft2");
+sum(square_fft == square_2_fft, 'all')
+
+
+square_3 = zeros(128, 128);
+[square_3_X, square_3_y] = meshgrid(24:103, 54:73);
+idx_3 = sub2ind(size(square_3), square_3_X, square_3_y);
+square_3(idx_3) = 1;
+square_3_fft = fft2(square_3);
+figure;
+subplot(1,2,1);imshow(square_3);title("square 3");
+subplot(1,2,2);imshow(abs(dip_fftshift(square_3_fft)));title("square 3 fft2");
+
+figure;
+subplot(1,3,1);imshow(abs(dip_fftshift(square_fft)));title("square1 fft2")
+subplot(1,3,2);imshow(abs(dip_fftshift(square_2_fft)));title("square2 fft2");
+subplot(1,3,3);imshow(abs(dip_fftshift(square_3_fft)));title("square 3 fft2");
+
+vec_80_1 = [zeros(24,1); ones(80,1); zeros(24,1)];
+vec_1_20 = [zeros(1,54), ones(1,20), zeros(1,54)];
+square_80_20 = vec_80_1 * vec_1_20;
+figure;sgtitle("the 2 vectors");
+subplot(1,2,1);imshow(vec_80_1);title('80x1 padded to 128x1');
+subplot(1,2,2);imshow(vec_1_20);title('1x20 padded to 1x128');
+
+figure;imshow(square_80_20);title("80x20 square by multiplying vectors");
+
+square_3_sep_fft = sep_fft2(vec_80_1, vec_1_20);
+figure;
+subplot(1,2,1);imshow(abs(dip_fftshift(square_3_fft)));title("square3 fft2")
+subplot(1,2,2);imshow(abs(dip_fftshift(square_3_sep_fft)));title("square3 fft2 by separating");
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Section 3
+%% Section 2
 beatles = imread("beatles.png");
 beatles_norm = double(rgb2gray(beatles))/255;
 beatles_dct2 = dct2(beatles_norm);
@@ -81,6 +133,29 @@ for a = [0.02 0.05 0.07 0.09 0.15]
     i = i+1;
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Section 3
+
+beetle = imread("beetle.jpg");
+beetle_norm = double(rgb2gray(beetle))/255;
+n=5;
+[c_beetle,s_beetle]=wavedec2(beetle_norm,n,'haar');
+
+
+for i = 1:n
+[h_beetle,v_beetle,detail_beetle] = detcoef2('all',c_beetle,s_beetle,i);
+cfs2 = appcoef2(c_beetle,s_beetle,'haar',i);
+V_img = wcodemat(v_beetle,255,'mat',i);
+H_img = wcodemat(h_beetle,255,'mat',i);
+D_img = wcodemat(detail_beetle,255,'mat',i);
+A_img = wcodemat(cfs2,255,'mat',i);
+figure;colormap('gray');
+subplot(2,2,1);imagesc(A_img);title(['Approximation Coef. of Level '  int2str(i)]);
+subplot(2,2,2);imagesc(H_img);title(['Horizontal Detail Coef. of Level '  int2str(i)]);
+subplot(2,2,3);imagesc(V_img);title(['Vertical Detail Coef. of Level '  int2str(i)]);
+subplot(2,2,4);imagesc(D_img);title(['Diagonal Detail Coef. of Level ' int2str(i)]);
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% functions
 function [img_fft2] = dip_fft2(I)
@@ -111,10 +186,15 @@ function [shifted_fft2] = dip_fftshift(F)
     quad_4 = F(1:M/2, N/2+1:end);
     shifted_fft2 = [quad_3 quad_2; quad_4 quad_1];
 end
+
 function [freed_willy] = Free_Willy(Willy)
     freed_willy = fft2(Willy);
     freed_willy(1, 1+10) = 0;
     freed_willy(1, 1+end-10) = 0;
     freed_willy = real(ifft2(freed_willy));
     figure;imshow(freed_willy);title("freed willy");
+end
+
+function [img_fft2] = sep_fft2(v1, v2)
+    img_fft2 = fft(v1) * fft(v2);
 end
