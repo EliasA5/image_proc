f989c70628b0506770146c9671400c0a59a5cdf8
Elias Assaf <elyas51000@gmail.com>
2021-12-10 15:23:36 +0200

    added hough circles algorithm

--- ex_4.m
+++ ex_4.m
@@ -5,7 +5,7 @@ disp('Elias Assaf 315284729 - Jameel Nassar 206985152')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Section 1.1
 cameraman = imread("cameraman.tif");
-cameraman = cameraman/256;
+cameraman = double(cameraman)/256;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Section 1.2
@@ -27,26 +27,15 @@ subplot(1,2,1);imshow(cameraman_canny_default);title(["default thresh hold = " n
 subplot(1,2,2);imshow(cameraman_canny_custom);title(["custom thresh hold = " num2str(canny_thresh)]);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Section 2
+%% Section 2.1
 
 floor = imread("floor.jpg");
 floor = rgb2gray(floor)/256;
 %default filter is sobel which we learned early in the course
-BW = edge(floor);
-%%%%%%%%
+BW_floor = edge(floor);
 
-[H,T,R] = hough(BW,'RhoResolution',1,'Theta',-90:1:89);
-figure;
-imshow(imadjust(rescale(H)),'XData',T,'YData',R,'InitialMagnification','fit');
-title('Hough transform of floor.jpg');
-xlabel('\theta'), ylabel('\rho');
-axis on, axis normal, hold on;
-colormap(gca,hot);
-
-%%%%%%%%%%%%
-
-[hough_mat_1_1,R_vec1,theta_vec1] = dip_hough_lines(BW,1,1);
-[hough_mat_5_4,R_vec2,theta_vec2] = dip_hough_lines(BW,5,4);
+[hough_mat_1_1,R_vec1,theta_vec1] = dip_hough_lines(BW_floor,1,1);
+[hough_mat_5_4,R_vec2,theta_vec2] = dip_hough_lines(BW_floor,5,4);
 figure;sgtitle('Hough transform of floor');
 subplot(1,2,1);imshow(imadjust(rescale(hough_mat_1_1)),'XData',theta_vec1,'YData',R_vec1, 'InitialMagnification','fit');
 title("R_0 = 1, \theta_0 = 1");
@@ -63,7 +52,25 @@ axis on, axis normal, hold on; colormap(gca,hot);
 peaks_1_1 = houghpeaks(hough_mat_1_1, 4);
 peaks_5_4 = houghpeaks(hough_mat_5_4, 4);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Section 2.2
 
+coffee = imread("coffee.jpg");
+coffee = double(rgb2gray(coffee))/256;
+
+BW_coffee = edge(coffee);
+
+tic;[hough_mat_circ_1_1, R_circ_vec1, theta_circ_vec1] = dip_hough_circles(BW_coffee, 1, 1);toc;
+[hough_mat_circ_4_10, R_circ_vec2, theta_circ_vec2] = dip_hough_circles(BW_coffee, 4, 10);
+hough_circ_peaks_1_1 = dip_houghpeaks3d(hough_mat_circ_1_1);
+hough_circ_peaks_4_10 = dip_houghpeaks3d(hough_mat_circ_4_10);
+
+figure;imshow(hough_mat_circ_1_1(:,:, 4), []);
+figure;imshow(coffee);hold on;
+viscircles(hough_circ_peaks_1_1(:, 1:2), R_circ_vec1(hough_circ_peaks_1_1(:, 3)),'EdgeColor','b');
+
+figure;imshow(coffee);hold on;
+viscircles(hough_circ_peaks_4_10(:, 1:2), R_circ_vec2(hough_circ_peaks_4_10(:, 3)),'EdgeColor','b');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -77,7 +84,7 @@ function [magnitude] = dip_prewitt_edge(img, thresh)
     magnitude = (magnitude >= thresh);
 end
 
-function [hough_mat,R_vec,theta_vec] = dip_hough_lines(BW, R_0, theta_0)
+function [hough_mat, R_vec, theta_vec] = dip_hough_lines(BW, R_0, theta_0)
     
     [M,N] = size(BW);
     R_value = round(sqrt(M^2 + N^2));
@@ -85,11 +92,12 @@ function [hough_mat,R_vec,theta_vec] = dip_hough_lines(BW, R_0, theta_0)
     theta_vec = -90:theta_0:90;
     hough_mat = zeros(length(R_vec),length(theta_vec));
     [x_vec, y_vec] = find(BW == 1);
-
+    x_vec = x_vec.';
+    y_vec = y_vec.';
 
     for i = 1:length(theta_vec)
         theta = theta_vec(i) * (pi / 180);
-        r = cos(theta) .* y_vec.' + sin(theta) .* x_vec.';
+        r = cos(theta) .* y_vec + sin(theta) .* x_vec;
         r = interp1(R_vec, R_vec, r, 'nearest');
         r = round((r+R_value + 1)/R_0);
         for j = r
@@ -97,4 +105,43 @@ function [hough_mat,R_vec,theta_vec] = dip_hough_lines(BW, R_0, theta_0)
         end
     end
 
+end
+
+function [hough_mat, R_vec, theta_vec] = dip_hough_circles(BW, R_0, theta_0)
+
+    [M,N] = size(BW);
+    R_vec = 80:R_0:100;
+    theta_vec = 0:theta_0:360-theta_0;
+    hough_mat = zeros(M, N, length(R_vec));
+    [x_vec, y_vec] = find(BW == 1);
+    x_vec = x_vec.';
+    y_vec = y_vec.';
+    for k = 1:length(R_vec)
+        r = R_vec(k);
+        for i = 1:length(theta_vec)
+            theta = theta_vec(i) * (pi / 180);
+            a = abs(round(x_vec - r*cos(theta)));
+            b = abs(round(y_vec - r*sin(theta)));
+            a_w = a(a>0 & b>0 & a<=M & b<=N);
+            b_w = b(a>0 & b>0 & a<=M & b<=N);
+            for j=1:length(a_w)
+                hough_mat(a_w(j), b_w(j), k) = hough_mat(a_w(j), b_w(j), k) + 1;
+            end
+        end
+    end
+
+end
+
+function [peaks] = dip_houghpeaks3d(H)
+    peaks = dip_houghpeaks3d_custom(H, 5);
+end
+
+function [peaks] = dip_houghpeaks3d_custom(H, numOfPeaks)
+    peaks = zeros(numOfPeaks, 3);
+    for i = 1:numOfPeaks
+        [val, idx] = max(H(:));
+        [idx1, idx2, idx3] = ind2sub(size(H), idx);
+        peaks(i, :) = [idx2, idx1, idx3];
+        H(idx1, idx2, idx3) = 0;
+    end
 end
