1759e03f61da9b6dc987485de36934e3471c99fa
jameel <jameelna978@gmail.com>
2021-12-30 20:25:42 +0200

    added some functions & changed figures & MLX

--- EX5.m
+++ EX5.m
@@ -119,9 +119,9 @@ angle_d = atan2((Distorted_ref.Location(:,2) - Distorted_nref.Location(:,2)),(Di
 avg_rotate = mean(angle_d - angle_o) * 180/pi;
 
 
-figure;
-showMatchedFeatures(straight_mona,crooked_mona,matchedOriginal,matchedDistorted);
-title('Putatively matched points (including outliers)');
+figure; ax = axes;
+showMatchedFeatures(straight_mona,crooked_mona,matchedOriginal,matchedDistorted,'montage','Parent',ax);
+title('matched features');
 legend('ptsOriginal','ptsDistorted');
 
 fixed_mona = imrotate(crooked_mona,avg_rotate);
@@ -146,8 +146,13 @@ for idx = 1:length(mona_images)
     img_name = mona_images{idx};
     mona_img_name = [pwd '\mona\' img_name];
     mona_fake = double(rgb2gray(imread(mona_img_name)))/256;
-    len = its_mona(mona_org,mona_fake);
-    fprintf('We found %i similar features points in %s \n', len,img_name);
+    [common_f,vpts1,vpts2] = is_mona(mona_org,mona_fake);
+    figure; ax = axes;
+    showMatchedFeatures(mona_org,mona_fake,vpts1,vpts2,'montage','Parent',ax);
+    title(ax, ['features of ' img_name ' | Number of mathced features = ' num2str(common_f) ]);
+    if(~isempty(vpts1))
+        legend(ax, 'Matched features 1','Matched features 2');
+    end
 end
 
 %%
@@ -158,63 +163,99 @@ figure;
 ID2QR(id1);
 
 %%
-qr_easy = imread('./11.jpg');
+qr_easy = imread('./1.png');
 qr_easy = double(rgb2gray(qr_easy))/256;
-figure;imshow(qr_easy);
-%qr_easy_corners = ginput(4);
-%qr_easy_corners = [3638 2598;2309 2732;3700 1140;2394 921];
-qr_easy_corners = [554 462;1867 336;485 1938;1786 2151];
+% figure;imshow(qr_easy);
+% qr_easy_corners = ginput(4);
+qr_easy_corners = [407 363;955 305;379 971;919 1061];
+[qr1,qr_mat1] = get_qr(qr_easy,qr_easy_corners);
+figure;
+subplot(1,3,1);imshow(qr_easy);hold on; plot(qr_easy_corners(:,1),qr_easy_corners(:,2),'r*');title('Original Image');
+subplot(1,3,2);imshow(qr1);title('QR image After projective transformation');
+subplot(1,3,3);imshow(qr_mat1);title('Detected QR Matrix');
+sgtitle('Image 1')
+fprintf('Restored id from image 1 is: ');
+fprintf('%g', get_id(qr_mat1));
+fprintf('\n');
 
 
-qr_medium = imread('./21.jpg');
+qr_medium = imread('./2.png');
 qr_medium = double(rgb2gray(qr_medium))/256;
-figure;imshow(qr_medium);
-%qr_medium_corners = ginput(4);
-%qr_medium_corners = [3433 2522;2701 2745;3482 1216;2773 935];
-qr_medium_corners = [490 497;1214 266;442 1811;1130 2090];
+% figure;imshow(qr_medium);
+% qr_medium_corners = ginput(4);
+qr_medium_corners = [489 391;793 297;471 937;759 1055];
+[qr2,qr_mat2] = get_qr(qr_medium,qr_medium_corners);
+figure;
+subplot(1,3,1);imshow(qr_medium);hold on; plot(qr_medium_corners(:,1),qr_medium_corners(:,2),'r*');title('Original Image');
+subplot(1,3,2);imshow(qr2);title('QR image After projective transformation');
+subplot(1,3,3);imshow(qr_mat2);title('Detected QR Matrix');
+sgtitle('Image 2')
+fprintf('Restored id from image 2 is: ');
+fprintf('%g', get_id(qr_mat1));
+fprintf('\n');
 
 
-qr_hard = imread('./31.jpg');
+qr_hard = imread('./3.png');
 qr_hard = double(rgb2gray(qr_hard))/256;
-figure;imshow(qr_hard);
-%qr_hard_corners = ginput(4);
-%qr_hard_corners = [3861 2772;3433 3129;3937 1278;3562 859];
-qr_hard_corners = [431 492;863 117;362 1983;728 2403];
+% figure;imshow(qr_hard);
+% qr_hard_corners = ginput(4);
+qr_hard_corners = [313 289;495 139;281 911;443 1093];
+[qr3,qr_mat3] = get_qr(qr_hard,qr_hard_corners);
+figure;
+subplot(1,3,1);imshow(qr_hard);hold on; plot(qr_hard_corners(:,1),qr_hard_corners(:,2),'r*');title('Original Image');
+subplot(1,3,2);imshow(qr3);title('QR image After projective transformation');
+subplot(1,3,3);imshow(qr_mat3);title('Detected QR Matrix');
+sgtitle('Image 3')
+fprintf('Restored id from image 3 is: ');
+fprintf('%g', get_id(qr_mat1));
+fprintf('\n');
 
 
 %%
-fixed_points = [1 1; 256 1; 1 256; 256 256];
-tform = fitgeotrans(fixed_points, qr_easy_corners, 'projective');
-invtform = invert(tform);
-out = imwarp(qr_easy, invtform, 'linear');
 
-figure;imshow(out);colormap gray;
-
-tform = fitgeotrans(fixed_points, qr_medium_corners, 'projective');
-invtform = invert(tform);
-out = imwarp(qr_medium, invtform, 'linear');
-
-figure;imshow(out);colormap gray;
-
-tform = fitgeotrans(fixed_points, qr_hard_corners, 'projective');
-invtform = invert(tform);
-out = imwarp(qr_hard, invtform, 'linear');
-
-figure;imshow(out);colormap gray;
-
-%%
-
-function same = its_mona(mona,img)
-
-same = 0;
-
-for num_octave=1:5
-mona_f = detectSURFFeatures(mona,'ROI', [80, 8, 80, 120],'NumOctaves', num_octave,'NumScaleLevels', 10,'MetricThreshold',300);
-img_f = detectSURFFeatures(img,'NumOctaves', num_octave,'NumScaleLevels', 10,'MetricThreshold',300);
-[featuresOriginal,  ~]  = extractFeatures(mona,  mona_f);
-[featuresDistorted, ~] = extractFeatures(img, img_f);
-indexPairs = matchFeatures(featuresOriginal, featuresDistorted);
-same = same + length(indexPairs);
+function [common_f,vpts1_vec,vpts2_vec] = is_mona(mona,img)
+common_f = 0;
+vpts1_vec = [];
+vpts2_vec = [];
+for octave_num=1:5
+mona_f = detectSURFFeatures(mona,'ROI', [80, 8, 80, 120],'NumOctaves', 6,'NumScaleLevels', 8,'MetricThreshold',500);
+img_f = detectSURFFeatures(img,'NumOctaves', 6,'NumScaleLevels', 8,'MetricThreshold',500);
+[f1, vpts1]  = extractFeatures(mona,  mona_f);
+[f2, vpts2] = extractFeatures(img, img_f);
+indexPairs = matchFeatures(f1, f2,"MaxRatio",0.8);
+common_f = common_f + length(indexPairs);
+if(isempty(vpts1_vec))
+    vpts1_vec = vpts1(indexPairs(:,1));
+    vpts2_vec = vpts2(indexPairs(:,1));
+else
+    vpts1_vec = [vpts1_vec ;vpts1(indexPairs(:,1))];
+    vpts2_vec = [vpts2_vec ;vpts2(indexPairs(:,1)) ];
+end
+end
 end
 
+function [qr,qr_mat] = get_qr(img,corners)
+fixed_points = [1 1; 384 1; 1 384; 384 384];
+tform = fitgeotrans(fixed_points, corners, 'projective');
+invtform = invert(tform);
+qr = imwarp(img, invtform, 'cubic','OutputView', imref2d( size(img) ));
+qr = qr(1:384,1:384);
+qr_mat = zeros(6,6);
+for i = 0:5
+    for j = 0:5
+        qr_mat(i+1,j+1) = qr(32 + i*64, 32 + j*64) > 0.4;
+    end
+end
+end
+
+function id  = get_id(bits)
+id = zeros(1,9);
+bits = bits(:);
+N = length(bits);
+
+for i=0:(N/4 - 1)
+    b = bits(1+4*i:(4*(i+1)));
+    num = bin2dec(sprintf('%d',b));
+    id(i+1) = num;
+end
 end
